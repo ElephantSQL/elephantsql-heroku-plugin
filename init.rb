90d101ab84d797271561f5d16e2bd972d15bbaca
Carl Hörberg <carl.hoerberg@gmail.com>
2014-01-06 14:22:52 +0100

    psql now takes option commands

--- init.rb
+++ init.rb
@@ -1,14 +1,25 @@
 require 'heroku/command/base'
 
+# manage elephantsql databases
+# 
 class Heroku::Command::ElephantSQL < Heroku::Command::Base
   def index
     display 'ElephantSQL'
   end
 
+  # elephantsql:psql
+  #
+  # -c, --command COMMAND # optional SQL command to run
+  #
+  # Open a psql shell to the database specified at ELEPHANTSQL_URL
+  #
   def psql
     vars = api.get_config_vars(app).body
     if url = vars['ELEPHANTSQL_URL']
-      exec "psql #{url}"
+      if command = options[:command]
+        command = "-c '#{command}'"
+      end
+      exec "psql #{command} #{url}"
     else
       error "No ElephantSQL database found"
     end
