ffd3ff8647db6551088414e2fe3a30216e9a69a3
Carl Hörberg <carl.hoerberg@gmail.com>
2014-01-09 11:11:48 +0100

    default to --create --no-owner and --no-acl for pg_dump

--- init.rb
+++ init.rb
@@ -31,11 +31,10 @@ class Heroku::Command::ElephantSQL < Heroku::Command::Base
   # Calls the pg_dump, can use all arguments as pg_dump can
   #
   def pg_dump
-    puts "args: #{args}"
     vars = api.get_config_vars(app).body
     if url = vars['ELEPHANTSQL_URL']
       u = URI.parse url
-      exec "PGPASSWORD=#{u.password} pg_dump -h #{u.host} -p #{u.port || 5432} -U #{u.user} -d #{u.path[1..-1]}"
+      exec "env PGPASSWORD=#{u.password} pg_dump -h #{u.host} -p #{u.port || 5432} -U #{u.user} -d #{u.path[1..-1]} -cOx"
     else
       error "No ElephantSQL database found"
     end
